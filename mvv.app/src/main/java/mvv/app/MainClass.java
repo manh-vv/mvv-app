8f1b904ff4d46e6761cea1c86b720e7ed4f7af32
Manh Vu <manh.vv.htth@gmail.com>
2015-06-21 04:17:35 +0700

    Update main

--- mvv.app/src/main/java/mvv/app/MainClass.java
+++ mvv.app/src/main/java/mvv/app/MainClass.java
@@ -1,5 +1,7 @@
 package mvv.app;
 
+import java.io.File;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +12,39 @@ public class MainClass {
 		long t1 = System.currentTimeMillis();
 		log.info("App start at {}, current directory: {}", t1, System.getProperty("user.dir"));
 
+		MainClass mainClass = new MainClass();
+		mainClass.onStart();
+		mainClass.run();
+		mainClass.onFinish();
+
 		long t2 = System.currentTimeMillis();
 		log.info("App end at {}; executed time: {}", t2, t2 - t1);
 	}
+
+    /**
+     *
+     * @author Manh Vu
+     */
+    private void onStart() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * @author Manh Vu
+     */
+    private void run() {
+        File fi = new File("cam-.txt");
+
+    }
+
+    /**
+     *
+     * @author Manh Vu
+     */
+    private void onFinish() {
+        // TODO Auto-generated method stub
+
+    }
 }
